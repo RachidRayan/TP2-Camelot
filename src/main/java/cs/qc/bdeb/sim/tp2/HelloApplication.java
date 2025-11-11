a356ee0b5b3d3332f636e7fa64f6a1ea4536b7e0
Gourramen <6320946@bdeb.qc.ca>
2025-11-11 10:34:24 -0500

    Initial commit

---
+++ src/main/java/cs/qc/bdeb/sim/tp2/HelloApplication.java
@@ -0,0 +1,23 @@
+package cs.qc.bdeb.sim.tp2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Scene scene = new Scene(new HBox(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module cs.qc.bdeb.sim.tp2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens cs.qc.bdeb.sim.tp2 to javafx.fxml;
+    exports cs.qc.bdeb.sim.tp2;
+}
