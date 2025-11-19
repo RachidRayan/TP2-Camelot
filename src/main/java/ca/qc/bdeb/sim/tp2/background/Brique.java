d9a052a4a38b0d1cad42a3764f4c61c3f85833af
RachidRayan <rachid.gourramen@gmail.com>
2025-11-19 15:12:07 -0500

    Animation Camelot, plus les touches, caméra, mise de méthode dans partie, tout marche mais ils me faut plus de vérifications à faire
    # Conflicts:
    #	src/main/java/ca/qc/bdeb/sim/tp2/JavaFX.java
    #	src/main/java/ca/qc/bdeb/sim/tp2/entites/Camelot.java

--- src/main/java/ca/qc/bdeb/sim/tp2/background/Brique.java
+++ src/main/java/ca/qc/bdeb/sim/tp2/background/Brique.java
@@ -1,44 +1,44 @@
-//package ca.qc.bdeb.sim.tp2.background;
-//
-//import ca.qc.bdeb.sim.tp2.JavaFX;
-//import javafx.scene.image.Image;
-//import javafx.scene.image.ImageView;
-//import javafx.scene.layout.HBox;
-//import javafx.scene.layout.VBox;
-//
-//public class Brique {
-//
-//    Image imageBrique = new Image("brique.png");
-//    ImageView brique = new ImageView(imageBrique);
-//
-//    HBox wall = rangeeBrique(cologneBrique());
-//    public HBox getWall() {
-//        return wall;
-//    }
-//
-//    public VBox cologneBrique(){
-//        VBox column = new VBox();
-//        double height=0;
-//
-//        while (height< JavaFX.h){
-//            column.getChildren().add(brique);
-//            height += brique.getY();
-//        }
-//
-//        return column;
-//    }
-//
-//    public HBox rangeeBrique(VBox cologneBrique){
-//        HBox row = new HBox();
-//        double with =0;
-//
-//        while(with<JavaFX.w){
-//            row.getChildren().add(cologneBrique);
-//            with += brique.getX();
-//        }
-//
-//        return row;
-//    }
-//
-//
-//}
+package ca.qc.bdeb.sim.tp2.background;
+
+import ca.qc.bdeb.sim.tp2.JavaFX;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class Brique {
+
+    Image imageBrique = new Image("brique.png");
+
+    HBox wall = RangeeBrique();
+    public HBox getWall() {
+        return wall;
+    }
+
+    public VBox CologneBrique(){
+        VBox column = new VBox();
+        double height=0;
+
+        while (height< JavaFX.h){
+            column.getChildren().add(new ImageView(imageBrique));
+            height += imageBrique.getHeight();
+        }
+
+        return column;
+    }
+
+    public HBox RangeeBrique(){
+        HBox row = new HBox();
+        double width =0;
+
+        while(width<JavaFX.w){
+            row.getChildren().add(CologneBrique());
+            width += imageBrique.getWidth();
+        }
+
+        return row;
+    }
+
+
+}
