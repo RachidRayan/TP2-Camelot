70e0e1925bf2cf1b91d2666e78b0c7dfa71e3d91
Daniil <danila.podvolotki@gmail.com>
2025-11-29 11:59:34 -0500

    Changement de style des portes

--- src/main/java/ca/qc/bdeb/sim/tp2/background/Maison.java
+++ src/main/java/ca/qc/bdeb/sim/tp2/background/Maison.java
@@ -7,39 +7,65 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Maison extends PlanArriere {
 
     private final double hauteurPorte = 250;
     private final double largeurPorte = 150;
-    private final double differencePositionnement = 2000; //difference entre les portes
+    private final double differencePositionnement = 1000; //difference entre les portes
 
-    private  int[] adresses = setAdresse();
+    private ArrayList<Integer> adresses = new ArrayList<>() ;
+    private ArrayList<Integer> adressesAbonnees = new ArrayList<>();
+    private ArrayList<Integer> adressesNonAbonnees = new ArrayList<>();
 
     public Maison(Camera camera) {
         super(camera);
+        setAdresses();
+        System.out.println(adresses);
+
+        System.out.println(adressesAbonnees);
+
+        System.out.println(adressesNonAbonnees);
     }
 
-    public int[] getAdresses() {
-        return adresses;
-    }
-    public void setAdresses(int[] adresses) {
-        this.adresses = adresses;
+    public ArrayList<Integer> getAdressesAbonnees() {
+        return adressesAbonnees;
     }
 
-    public int[] setAdresse(){
+    public ArrayList<Integer> getAdressesNonAbonnees() {
+        return adressesNonAbonnees;
+    }
+
+
+    public void setAdresses(){
         Random random = new Random();
-        int troisiemeChiffre = random.nextInt(8);
 
-        int[] numbers = new int[8];
-        for (int i = 0; i < numbers.length; i++) {
-            Random r = new Random();
-            numbers[i] = r.nextInt(100);
-            numbers[i] += troisiemeChiffre*100;
+        int premiereAdresse = random.nextInt(100,950);
+        adresses.add(premiereAdresse);
+        verifivcationAbonnement(premiereAdresse);
+
+        int nombreAdressesRestant = 11;
+
+        for (int i = 0; i < nombreAdressesRestant; i++) {
+            int adresseProchaine = premiereAdresse + 2;
+            premiereAdresse = adresseProchaine;
+            adresses.add(adresseProchaine);
+            verifivcationAbonnement(adresseProchaine);
         }
 
-        return numbers;
+    }
+
+    public void verifivcationAbonnement(int adresse) {
+        Random chanceRandom = new Random();
+        int chance = chanceRandom.nextInt(0,2);
+        if (chance == 1) {
+            adressesAbonnees.add(adresse);
+        }
+        else {
+            adressesNonAbonnees.add(adresse);
+        }
     }
 
 
@@ -51,16 +77,24 @@ public class Maison extends PlanArriere {
     public void draw(GraphicsContext context) {
         Point2D positionEcran = camera.coordoEcran(position);
 
-
-        for (int i = 0; i < adresses.length; i++) {
-            int number = adresses[i];
-
+        for (int adresse : adresses) {
             context.setFill(Color.BROWN);
-            context.fillRect(positionEcran.getX() + differencePositionnement *i, JavaFX.h- hauteurPorte, largeurPorte, hauteurPorte);
+            context.fillRect(positionEcran.getX() + differencePositionnement , JavaFX.h- hauteurPorte, largeurPorte, hauteurPorte);
 
             context.setFill(Color.YELLOW);
             context.setFont(new Font(80));
-            context.fillText(String.valueOf(adresses[i]),positionEcran.getX() + differencePositionnement *i,JavaFX.h- hauteurPorte);
+            context.fillText(String.valueOf(adresse),positionEcran.getX() + differencePositionnement,JavaFX.h- hauteurPorte);
         }
+//
+//        for (int i = 0; i < nombreAdressesTotal; i++) {
+////            int number = adresses[i];
+//
+//            context.setFill(Color.BROWN);
+//            context.fillRect(positionEcran.getX() + differencePositionnement *i, JavaFX.h- hauteurPorte, largeurPorte, hauteurPorte);
+//
+//            context.setFill(Color.YELLOW);
+//            context.setFont(new Font(80));
+//            context.fillText(String.valueOf(adresses.indexOf(i)),positionEcran.getX() + differencePositionnement *i,JavaFX.h- hauteurPorte);
+//        }
     }
 }
