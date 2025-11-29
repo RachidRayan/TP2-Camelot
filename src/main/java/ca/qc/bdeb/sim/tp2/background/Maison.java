2d5786a099f024abb1f1823cca86b176a6e59428
Daniil <danila.podvolotki@gmail.com>
2025-11-29 12:11:12 -0500

    Les portes marchent comme il faut

--- src/main/java/ca/qc/bdeb/sim/tp2/background/Maison.java
+++ src/main/java/ca/qc/bdeb/sim/tp2/background/Maison.java
@@ -14,7 +14,7 @@ public class Maison extends PlanArriere {
 
     private final double hauteurPorte = 250;
     private final double largeurPorte = 150;
-    private final double differencePositionnement = 1000; //difference entre les portes
+    private final double differencePositionnement = 2000; //difference entre les portes
 
     private ArrayList<Integer> adresses = new ArrayList<>() ;
     private ArrayList<Integer> adressesAbonnees = new ArrayList<>();
@@ -68,33 +68,19 @@ public class Maison extends PlanArriere {
         }
     }
 
-
-//    public void update(double deltaTemps){
-//        position = position.add(velocite.multiply(deltaTemps));
-//    }
-
-    //Porte avec adresse
+    // Dessin des portes avec adresse
     public void draw(GraphicsContext context) {
         Point2D positionEcran = camera.coordoEcran(position);
+        int sautAdresse = 1;
 
         for (int adresse : adresses) {
             context.setFill(Color.BROWN);
-            context.fillRect(positionEcran.getX() + differencePositionnement , JavaFX.h- hauteurPorte, largeurPorte, hauteurPorte);
+            context.fillRect(positionEcran.getX() + differencePositionnement * sautAdresse, JavaFX.h- hauteurPorte, largeurPorte, hauteurPorte);
 
             context.setFill(Color.YELLOW);
-            context.setFont(new Font(80));
-            context.fillText(String.valueOf(adresse),positionEcran.getX() + differencePositionnement,JavaFX.h- hauteurPorte);
+            context.setFont(new Font(60));
+            context.fillText(String.valueOf(adresse),positionEcran.getX() + differencePositionnement * sautAdresse,JavaFX.h- hauteurPorte);
+            sautAdresse++;
         }
-//
-//        for (int i = 0; i < nombreAdressesTotal; i++) {
-////            int number = adresses[i];
-//
-//            context.setFill(Color.BROWN);
-//            context.fillRect(positionEcran.getX() + differencePositionnement *i, JavaFX.h- hauteurPorte, largeurPorte, hauteurPorte);
-//
-//            context.setFill(Color.YELLOW);
-//            context.setFont(new Font(80));
-//            context.fillText(String.valueOf(adresses.indexOf(i)),positionEcran.getX() + differencePositionnement *i,JavaFX.h- hauteurPorte);
-//        }
     }
 }
